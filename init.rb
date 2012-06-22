a0e37d543bd9a652c2763244bad308f3b1c2020c
Kazuki Kitabayashi <kazukeyan+github@gmail.com>
2012-06-22 13:52:31 +0900

    call_hookでの処理からafter_saveでの処理への移行

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_issues_on_google_calendar do
   name 'Redmine Issues On Google Calendar plugin'
   author 'Kazuki Kitabayashi'
   description 'This is a plugin for Redmine'
-  version '0.1.0'
+  version '0.1.4'
   url 'https://github.com/kazukeyan/redmine_issues_on_google_calendar'
   author_url 'https://github.com/kazukeyan/'
 end
@@ -18,23 +18,8 @@ Dispatcher.to_prepare :redmine_issues_on_google_calendar do
     Issue.send(:include, RedmineIssuesOnGoogleCalendar::IssuePatch)
   end
 
-  require_dependency 'issues_controller'
-  unless IssuesController.included_modules.include? RedmineIssuesOnGoogleCalendar::IssuesControllerPatch
-    IssuesController.send(:include, RedmineIssuesOnGoogleCalendar::IssuesControllerPatch)
-  end
-
   require_dependency 'project'
   unless Project.included_modules.include? RedmineIssuesOnGoogleCalendar::ProjectPatch
     Project.send(:include, RedmineIssuesOnGoogleCalendar::ProjectPatch)
   end
-
-  require_dependency 'projects_controller'
-  unless ProjectsController.included_modules.include? RedmineIssuesOnGoogleCalendar::ProjectsControllerPatch
-    ProjectsController.send(:include, RedmineIssuesOnGoogleCalendar::ProjectsControllerPatch)
-  end
 end
-
-require 'redmine_issues_on_google_calendar/hooks/controller_issues_new_after_save_hook'
-require 'redmine_issues_on_google_calendar/hooks/controller_issues_update_after_save_hook'
-require 'redmine_issues_on_google_calendar/hooks/controller_projects_new_after_save_hook'
-require 'redmine_issues_on_google_calendar/hooks/controller_projects_update_after_save_hook'
--- lib/redmine_issues_on_google_calendar/hooks/controller_issues_update_after_save_hook.rb
+++
@@ -1,9 +0,0 @@
-module RedmineIssuesOnGoogleCalendar
-  module Hooks
-    class ControllerIssuesUpdateAfterSaveHook < Redmine::Hook::ViewListener
-      def controller_issues_update_after_save(context={})
-        context[:issue].update_google_calendar_event
-      end
-    end
-  end
-end
